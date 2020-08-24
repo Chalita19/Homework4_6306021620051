c5f3777c8668a50398ac6356cb3020293d6bedde
Chalita19 <68455899+Chalita19@users.noreply.github.com>
2020-08-24 16:35:20 +0700

    Add files via upload

---
+++ Homework4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int Calage(int);
+int main()
+{
+	int Year,age;
+	for (int i = 1 ; i <= 3 ; i++){ 
+     cout<<"Enter Year" <<i<< ":";
+	 cin>> Year ;
+	 int age = Calage (Year) ;
+	 cout << "age" <<i<< ":" << age <<endl;
+	}
+	return (0);
+}
+int Calage (int Y)
+{   int age;
+	age = 2563-Y ;
+	return (age);
+} 
